14b66502496fc5a88b99620ed3fa0314ed383d8c
root <root@pythonserver.pythonserver.h5.internal.cloudapp.net>
2014-11-04 07:57:30 +0000

    current stable version

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,7 +1,7 @@
 # Django settings for mysite project.
 import os
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -24,7 +24,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['http://pythonserver.cloudapp.net']
+ALLOWED_HOSTS = ['pythonserver.cloudapp.net']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/azureuser/Projects/pythonserver/mysite/userregister/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
